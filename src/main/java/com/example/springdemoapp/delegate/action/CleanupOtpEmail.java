a7a95bd7b66ace34f2b856b570541ea1de984042
AmineNaboulsi <naboulsiiamine@gmail.com>
2025-05-29 10:12:46 +0100

    feat: adding timers otps

--- src/main/java/com/example/springdemoapp/delegate/action/CleanupOtpEmail.java
+++ src/main/java/com/example/springdemoapp/delegate/action/CleanupOtpEmail.java
@@ -1,4 +1,19 @@
 package com.example.springdemoapp.delegate.action;
 
-public class CleanupOtpEmail {
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.LoggerFactory;
+
+public class CleanupOtpEmail implements JavaDelegate {
+
+    private final org.slf4j.Logger logger = LoggerFactory.getLogger(CreateCompany.class);
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+
+        delegateExecution.removeVariable("email-OTP-code");
+        delegateExecution.removeVariable("email-Otp-CreatedAt");
+
+        logger.info("Clean up Otp Email Work " );
+    }
 }
